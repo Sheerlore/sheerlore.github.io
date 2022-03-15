b954ec1f90cf44d78cd2c4d2cf9e555398e27f7a
uai <noema01@outlook.jp>
2022-03-15 20:34:07 +0900

    add nuxt project

---
+++ uai_website/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="container">
+    <h1>{{ githubData.name }} Website</h1>
+    <nav>
+      <li>{{ githubData.login }}</li>
+      <li>{{ githubData.avatar_url }}</li>
+      <li>{{ githubData.html_url }}</li>
+      <li>{{ githubData.blog }}</li>
+      <li>{{ githubData.bio }}</li>
+      <li>{{ githubData.twitter_username }}</li>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+
+  async asyncData () {
+    const res = await axios.get('https://api.github.com/users/Sheerlore')
+    return { githubData: res.data }
+  }
+}
+</script>
